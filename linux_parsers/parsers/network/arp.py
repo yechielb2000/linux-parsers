f34f940c3e60f563e829b02615ec77dac022a68f
Yechiel Babani <yechielb2000@gmail.com>
2025-03-05 19:49:17 +0200

    update arp docstring and name

--- linux_parsers/parsers/network/arp.py
+++ linux_parsers/parsers/network/arp.py
@@ -1,8 +1,13 @@
 import re
 
 
-def parse_arp_i(command_output: str) -> list[dict[str, str]]:
-    """parse `arp -i <interface>`"""
+def parse_arp(command_output: str) -> list[dict[str, str]]:
+    """
+    parse arp commands
+    `arp -i <interface>`.
+    `arp -en`.
+    `arp -e`.
+    """
     pattern = (
         r'^(?P<Address>\S+)\b\s+'
         r'(?P<HWtype>\S+)\b\s+'
--- tests/parsers/network/arp.py
+++ tests/parsers/network/arp.py
@@ -1,4 +1,4 @@
-from linux_parsers.parsers.network.arp import parse_arp_i
+from linux_parsers.parsers.network.arp import parse_arp
 
 
 def test_arp_i():
@@ -8,7 +8,7 @@ Address                  HWtype  HWaddress           Flags Mask            Iface
 192.168.1.100            ether   11:22:33:44:55:66   C                     eth0
 192.168.1.101            ether   aa:bb:cc:dd:ee:ff   C                     eth0
     """
-    result = parse_arp_i(output)
+    result = parse_arp(output)
     expected = [
         {
             'Address': '192.168.1.1', 'FlagsMask': 'C', 'HWaddress': '00:1a:2b:3c:4d:5e', 'HWtype': 'ether',
