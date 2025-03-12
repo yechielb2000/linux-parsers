b696bb77c2539876a89c5b6a22ffebd9972aa182
Yechiel Babani <yechielb2000@gmail.com>
2025-03-12 22:09:27 +0200

    add hwinfo

---
+++ linux_parsers/parsers/system/hwinfo.py
@@ -0,0 +1,23 @@
+import re
+
+
+def parse_hwinfo(command_output: str) -> list[dict]:
+    """Parse `hwinfo --<action>` command outputs.
+        CPU	                 hwinfo --cpu
+        Memory (RAM)	     hwinfo --memory
+        Disks (HDD/SSD)	     hwinfo --disk
+        Network Interfaces	 hwinfo --network
+        USB Devices	         hwinfo --usb
+        GPU (Graphics)	     hwinfo --gfxcard
+        Bluetooth	         hwinfo --bluetooth
+    """
+    blocks = re.split(r'^\d+:', command_output, flags=re.MULTILINE)
+    parsed_command = []
+    for block in list(filter(str.strip, blocks)):
+        fields = [i.strip() for i in block.splitlines() if i.strip()][2:]
+        block_fields = {}
+        for field in fields:
+            key, value = field.split(':')
+            block_fields[key.strip()] = value.strip()
+        parsed_command.append(block_fields)
+    return parsed_command
