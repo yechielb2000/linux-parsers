aed55e0a3d9d42548c6cfdcfb2ee30eefe80f7ff
Yechiel Babani <yechielb2000@gmail.com>
2025-03-16 20:02:38 +0200

    parse w command

---
+++ linux_parsers/parsers/session/w.py
@@ -0,0 +1,27 @@
+import re
+
+
+def parse_w(command_output: str) -> dict[str, any]:
+    """Parse `w` command output."""
+    metadata_pattern = re.compile(
+        r'(?P<time>\S+)\D+(?P<uptime>\S+),\s+(?P<users>\d+)\D+'
+        r'(?P<load1>\S+),\s+(?P<load2>\S+),\s+(?P<load3>\S+)'
+    )
+    record_pattern = re.compile(
+        r'^(?P<user>\S+)\s+(?P<tty>\S+)\s+(?P<from>\S+)\s+(?P<login>\S+)\s+'
+        r'(?P<idle>\S+)\s+(?P<jcpu>\S+)\s+(?P<pcpu>\S+)\s+(?P<command>.+)$',
+        re.MULTILINE
+    )
+    parsed_output = {'records': []}
+    lines = command_output.splitlines()
+    while lines:
+        line = lines.pop(0)
+        if 'load average' in line:
+            parsed_output['metadata'] = metadata_pattern.search(line).groupdict()
+        elif line.startswith('USER') and 'LOGIN@' in line:
+            continue
+        else:
+            regex_result = record_pattern.search(line)
+            if regex_result:
+                parsed_output['records'].append(regex_result.groupdict())
+    return parsed_output
---
+++ tests/session/test_w.py
@@ -0,0 +1,17 @@
+from linux_parsers.parsers.session.w import parse_w
+
+
+def test_w():
+    command_output = """
+     16:25:11 up  2:35,  4 users,  load average: 0.21, 0.19, 0.14
+USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
+alice    pts/0    192.168.1.100    14:03    1:25   0.20s  0.10s bash
+alice    pts/1    192.168.1.101    14:30    0.00s  0.05s  0.02s vim file.txt
+"""
+    parsed_output = parse_w(command_output)
+    assert len(parsed_output['records']) == 2
+    assert parsed_output['metadata'] == {'load1': '0.21', 'load2': '0.19', 'load3': '0.14',
+                                         'time': '16:25:11', 'uptime': '2:35', 'users': '4'}
+    assert parsed_output['records'][1] == {'command': 'vim file.txt', 'from': '192.168.1.101', 'idle': '0.00s',
+                                           'jcpu': '0.05s', 'login': '14:30', 'pcpu': '0.02s', 'tty': 'pts/1',
+                                           'user': 'alice'}
