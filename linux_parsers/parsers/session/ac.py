f916fc0ffb039ebac6873054f73d6995d4492861
Yechiel Babani <yechielb2000@gmail.com>
2025-04-05 20:16:37 +0300

    add ac -d parser

---
+++ linux_parsers/parsers/session/ac.py
@@ -0,0 +1,17 @@
+import re
+from typing import Dict
+
+
+def ac_d(command_output: str) -> Dict[str, str]:
+    user_data_pattern = re.compile(r"\s*(?P<user>\S+)\s+(?P<time>[\d.]+)")
+    parsed_output = {"users": [], "total": None}
+    lines = [i.strip() for i in command_output.splitlines() if i.strip()]
+    while lines:
+        line = lines.pop(0)
+        if line.startswith("total"):
+            parsed_output["total"] = line.split()[1].strip()
+            continue
+        regex_result = user_data_pattern.search(line)
+        if regex_result:
+            parsed_output["users"].append(regex_result.groupdict())
+    return parsed_output
