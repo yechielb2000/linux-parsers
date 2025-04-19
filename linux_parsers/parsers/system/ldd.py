21472d7c563fdc21d0c1353638217e268d15e8e1
Yechiel Babani <yechielb2000@gmail.com>
2025-04-19 22:51:08 +0300

    polish

--- linux_parsers/parsers/system/ldd.py
+++ linux_parsers/parsers/system/ldd.py
@@ -1,5 +1,6 @@
 import re
-from typing import List, Dict, Any, Optional
+from collections import defaultdict
+from typing import List, Dict, Any, Union
 
 
 def parse_ldd(command_output: str) -> List[Dict[str, Any]]:
@@ -8,9 +9,32 @@ def parse_ldd(command_output: str) -> List[Dict[str, Any]]:
     return [i.groupdict() for i in pattern.finditer(command_output)]
 
 
-def parse_ldd_verbose(command_output: str) -> Optional[str]:
+def parse_ldd_verbose(command_output: str) -> Dict[str, Union[List, Dict]]:
     """Parse `ldd -v <program>` command output."""
-    pass
+    library_pattern = re.compile(r"\s*(?P<program>\S+)\s+(?:=>\s(?P<path>\S+)\s+)?\((?P<address>\S+)\)")
+    version_pattern = re.compile(r"\s*(?P<dependency>\S+)\s+\(GLIBC_(?P<version>\S+)\)\s+=>\s+(?P<resolved_path>.+)")
+    lines = command_output.splitlines()
+    parsing_versions = False
+    current_lib = None
+    parsed_output = {"libraries": [], "versions": defaultdict(list)}
+    while lines:
+        line = lines.pop(0)
+        if line.strip() == "Version information:":
+            parsing_versions = True
+            continue
+        if parsing_versions:
+            match_lib = re.match(r"\s*(\S+):", line)
+            if match_lib:
+                current_lib = match_lib.group(1)
+                continue
+            regex_result = version_pattern.search(line)
+            if regex_result:
+                parsed_output["versions"][current_lib].append(regex_result.groupdict())
+        else:
+            regex_result = library_pattern.search(line)
+            if regex_result:
+                parsed_output["libraries"].append(regex_result.groupdict())
+    return parsed_output
 
 
 def parse_ldd_version(command_output: str) -> List[Dict[str, Any]]:
--- tests/system/test_ldd.py
+++ tests/system/test_ldd.py
@@ -1,4 +1,4 @@
-from linux_parsers.parsers.system.ldd import parse_ldd, parse_ldd_version
+from linux_parsers.parsers.system.ldd import parse_ldd, parse_ldd_version, parse_ldd_verbose
 
 
 def test_ldd():
@@ -29,3 +29,33 @@ Written by Roland McGrath and Ulrich Drepper.
 """
     parsed_output = parse_ldd_version(command_output)
     assert parsed_output == "2.31"
+
+
+def test_ldd_verbose():
+    command_output = """
+        linux-vdso.so.1 (0x00007fff31cd9000)
+        libm.so.6 => /lib/x86_64-linux-gnu/libm.so.6 (0x00007fded7548000)
+        /lib64/ld-linux-x86-64.so.2 (0x00007fded7670000)
+
+        Version information:
+        /bin/awk:
+                libm.so.6 (GLIBC_2.38) => /lib/x86_64-linux-gnu/libm.so.6
+                libm.so.6 (GLIBC_2.2.5) => /lib/x86_64-linux-gnu/libm.so.6
+                libc.so.6 (GLIBC_2.3) => /lib/x86_64-linux-gnu/libc.so.6
+        /lib/x86_64-linux-gnu/libm.so.6:
+                ld-linux-x86-64.so.2 (GLIBC_PRIVATE) => /lib64/ld-linux-x86-64.so.2
+                libc.so.6 (GLIBC_PRIVATE) => /lib/x86_64-linux-gnu/libc.so.6
+        /lib/x86_64-linux-gnu/libc.so.6:
+                ld-linux-x86-64.so.2 (GLIBC_2.35) => /lib64/ld-linux-x86-64.so.2
+"""
+    parsed_output = parse_ldd_verbose(command_output)
+    assert parsed_output["versions"]["/bin/awk"][0] == {
+        "dependency": "libm.so.6",
+        "resolved_path": "/lib/x86_64-linux-gnu/libm.so.6",
+        "version": "2.38",
+    }
+    assert parsed_output["libraries"][0] == {
+        "address": "0x00007fff31cd9000",
+        "path": None,
+        "program": "linux-vdso.so.1",
+    }
